344c4b188acd3c1f6bc89c234b199acb66954003
WagdySamih <wagdysamih787@gmail.com>
2023-01-02 18:56:52 +0200

    fix: changes content of her section

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -7,11 +7,18 @@ const Hero: React.FC = () => {
       <h1 className={styles.name}>Wagdy Samih.</h1>
       <div className={styles.intro}>I build things for the web.</div>
       <p className={styles.text}>
-        I am software engineer, specialized in front-end development
+        I am software engineer, Working mostly with JavaScript frameworks
         <br />
-        I have more than one year of Experience in react, react native.
-        <br />
-        Also I&rsquo;m ITI graduate
+        I have more than two year of Experience as MEAN | MERN stack developer
+        <br />& right now I am working as software developer at{" "}
+        <a
+          href="https://www.cultofcoders.com/"
+          target={"_blank"}
+          rel="noreferrer"
+        >
+          Cult of coders
+        </a>
+        .
       </p>
     </div>
   );
