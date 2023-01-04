cd2f8578f7e2902e37b31960d7c6dacde0f15532
WagdySamih <wagdysamih787@gmail.com>
2023-01-04 23:24:52 +0200

    fix: change footer text

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,7 +1,11 @@
 import styles from "./Footer.module.scss";
 
 const Footer: React.FC = () => {
-  return <footer className={styles.container}>Made by Wagdy Samih</footer>;
+  return (
+    <footer className={styles.container}>
+      Made by Wagdy Samih & Inspired by the others.
+    </footer>
+  );
 };
 
 export default Footer;
