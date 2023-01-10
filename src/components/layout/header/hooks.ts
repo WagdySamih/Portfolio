dc2f5d3cd78f3c5a7f8926ff9647e20e250ae96e
WagdySamih <wagdysamih787@gmail.com>
2023-01-10 03:33:17 +0200

    refactor: header scroll hook and add offset to remove header

--- src/components/layout/header/hooks.ts
+++ src/components/layout/header/hooks.ts
@@ -1,34 +1,41 @@
 import { useEffect } from "react";
 import styles from "./Header.module.scss";
+const scrollUp = styles.scrollUp;
+const scrollDown = styles.scrollDown;
+let lastScroll = 0;
+const HideHeaderOffset = 500;
 
 export const useHeaderAnimate = () => {
   useEffect(() => {
     if (!document || !window) return;
-
-    const body = document.body;
-    const scrollUp = styles.scrollUp;
-    const scrollDown = styles.scrollDown;
-    let lastScroll = 0;
-
-    window.addEventListener("scroll", () => {
-      const currentScroll = window.pageYOffset;
-      if (currentScroll <= 0) {
-        body.classList.remove(scrollUp);
-        return;
-      }
-
-      if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
-        // down
-        body.classList.remove(scrollUp);
-        body.classList.add(scrollDown);
-      } else if (
-        currentScroll < lastScroll &&
-        body.classList.contains(scrollDown)
-      ) {
-        body.classList.remove(scrollDown);
-        body.classList.add(scrollUp);
-      }
-      lastScroll = currentScroll;
-    });
+    window.addEventListener("scroll", HandleScroll);
   }, []);
+};
+
+const HandleScroll = () => {
+  const body = document.body;
+
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove(scrollUp);
+    return;
+  }
+
+  const shouldRemoveHeader =
+    currentScroll > HideHeaderOffset &&
+    currentScroll > lastScroll &&
+    !body.classList.contains(scrollDown);
+
+  const shouldDisplayHeader =
+    currentScroll < lastScroll && body.classList.contains(scrollDown);
+
+  if (shouldRemoveHeader) {
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+  } else if (shouldDisplayHeader) {
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+  }
+
+  lastScroll = currentScroll;
 };
