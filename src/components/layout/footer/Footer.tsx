a784aaad54070ddae4b00d12520d94e48136604c
WagdySamih <wagdysamih787@gmail.com>
2023-01-10 10:52:46 +0200

    feat: adds social links to footer in tablet/ mobile view

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,8 +1,38 @@
+import { FacebookIcon, GitHubIcon, LinkedInIcon } from "@assets";
 import styles from "./Footer.module.scss";
 
 const Footer: React.FC = () => {
   return (
     <footer className={styles.container}>
+      <ul className={styles.social}>
+        <li>
+          <a
+            href="https://github.com/WagdySamih"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            <GitHubIcon/>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/wagdy-samih/"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            <LinkedInIcon />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/profile.php?id=100006375610497"
+            target={"_blank"}
+            rel="noreferrer"
+          >
+            <FacebookIcon />
+          </a>
+        </li>
+      </ul>
       Made by Wagdy Samih & Inspired by the others.
     </footer>
   );
