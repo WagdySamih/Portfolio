e69d3e4b176e190ea22a562fdbf685d36c17bedb
WagdySamih <wagdysamih787@gmail.com>
2023-01-19 03:26:49 +0200

    fix: failed to optimize images build error

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
-require('dotenv').config({path:`.env`});
+require('dotenv').config({ path: `.env` });
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   env: {
     YOUR_VARIABLE_NAME: process.env.YOUR_VARIABLE_NAME
   },
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
