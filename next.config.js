acca6451c5236afbe50ce20ee149a9305b2c8745
WagdySamih <wagdysamih787@gmail.com>
2023-01-19 03:11:01 +0200

    fix: install dotenv for handling env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+require('dotenv').config({path:`.env`});
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    YOUR_VARIABLE_NAME: process.env.YOUR_VARIABLE_NAME
+  },
 }
 
 module.exports = nextConfig
