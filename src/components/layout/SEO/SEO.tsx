f6f59e7cbeb36c630bc37ffa5a3ac1a5de8297b9
Wagdy Samih <WagdySamih787@gmail.com>
2024-05-19 22:44:44 +0300

    chore: update portfolio hero section and website description and meta tags

--- src/components/layout/SEO/SEO.tsx
+++ src/components/layout/SEO/SEO.tsx
@@ -3,39 +3,39 @@ import Head from "next/head";
 const WEBSITE_NAME = "Wagdy Samih | Portfolio";
 const AUTHOR_NAME = "Wagdy Samih";
 const WEBSITE_DESCRIPTION =
-  "Wagdy Samih is software engineer, Working mostly with JavaScript frameworks, Having +3 year of Experience as MEAN | MERN stack developer";
+  "Wagdy Samih is software engineer, Working mostly with JavaScript frameworks, Having four year of Experience as MERN | MEAN stack developer";
 
 const WEBSITE_IMAGE = "images/demo.png";
 const WEBSITE_KEYWORDS =
-  "Wagdy, Wagdy Samih, Wagdy Samih Software engineer, JavaScript Developer, Full Stack developer, MEARN Stack developer";
+  "Wagdy, Wagdy Samih, Wagdy Samih Software engineer, JavaScript Developer, Full Stack developer, MEARN Stack developer, MERN Stack developer, Next.js developer";
 
 const SEO: React.FC = () => {
   return (
     <Head>
-      <meta charSet="utf-8" />
+      <meta charSet='utf-8' />
       <title>{WEBSITE_NAME}</title>
-      <meta name="description" content={WEBSITE_DESCRIPTION} />
-      <meta itemProp="name" content={WEBSITE_NAME} />
-      <meta itemProp="description" content={WEBSITE_DESCRIPTION} />
-      <meta name="keywords" content={WEBSITE_KEYWORDS} />
-      <meta name="author" content={AUTHOR_NAME} />
-      <meta name="theme-color" content="#0a192f" />
-      <link rel="preconnect" href="https://www.google-analytics.com"></link>
-      <link rel="dns-prefetch" href="https://www.google-analytics.com"></link>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta itemProp="image" content={WEBSITE_IMAGE} />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content={WEBSITE_NAME} />
-      <meta property="og:image" content={WEBSITE_IMAGE} />
-      <meta property="og:description" content={WEBSITE_DESCRIPTION} />
-      <meta property="og:site_name" content={WEBSITE_NAME} />
+      <meta name='description' content={WEBSITE_DESCRIPTION} />
+      <meta itemProp='name' content={WEBSITE_NAME} />
+      <meta itemProp='description' content={WEBSITE_DESCRIPTION} />
+      <meta name='keywords' content={WEBSITE_KEYWORDS} />
+      <meta name='author' content={AUTHOR_NAME} />
+      <meta name='theme-color' content='#0a192f' />
+      <link rel='preconnect' href='https://www.google-analytics.com'></link>
+      <link rel='dns-prefetch' href='https://www.google-analytics.com'></link>
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta itemProp='image' content={WEBSITE_IMAGE} />
+      <meta property='og:type' content='website' />
+      <meta property='og:title' content={WEBSITE_NAME} />
+      <meta property='og:image' content={WEBSITE_IMAGE} />
+      <meta property='og:description' content={WEBSITE_DESCRIPTION} />
+      <meta property='og:site_name' content={WEBSITE_NAME} />
 
-      <meta name="twitter:title" content={WEBSITE_NAME} />
-      <meta name="twitter:description" content={WEBSITE_DESCRIPTION} />
-      <meta name="twitter:image" content={WEBSITE_IMAGE} />
+      <meta name='twitter:title' content={WEBSITE_NAME} />
+      <meta name='twitter:description' content={WEBSITE_DESCRIPTION} />
+      <meta name='twitter:image' content={WEBSITE_IMAGE} />
       <meta
-        name="google-site-verification"
-        content="Wk8A0i8o745pE3he5ZdekszJShm0GoS8pPBBDCWr0uI"
+        name='google-site-verification'
+        content='Wk8A0i8o745pE3he5ZdekszJShm0GoS8pPBBDCWr0uI'
       />
     </Head>
   );
--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -1,25 +1,23 @@
 import { Section } from "components/UI";
 import styles from "./Hero.module.scss";
 
-
-
 const Hero: React.FC = () => {
   return (
     <Section className={styles.container}>
-      <div className={styles.hi}>Hi, My name is</div>
+      <div className={styles.hi}>Hi, I&lsquo;m</div>
       <h1 className={styles.name}>Wagdy Samih.</h1>
       <div className={styles.intro}>I am a software developer.</div>
       <p className={styles.text}>
         Working mostly with JavaScript frameworks
         <br />
-        I have almost three years of experience as a MEAN | MERN stack developer.
+        I have four years of experience as a MEAN | MERN stack developer.
         <br />& right now, I am working as a software developer at{" "}
         <a
-          href="https://www.cultofcoders.com/"
+          href='https://crypto.oanda.com/uk-en/'
           target={"_blank"}
-          rel="noreferrer"
+          rel='noreferrer'
         >
-          Cult of coders
+          Oanda Crypto (Coinpass)
         </a>
         .
       </p>
